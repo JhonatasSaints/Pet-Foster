abd794a3ecb789fb54711a5b0e6249807d164495
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-01-04 19:03:14 -0300

    third commit
    Login.Page.dart
    - - added, cpf / password and a button with no action.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:pet_foster/pages/login.page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
   class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pet Foster',
-      theme: ThemeData(primarySwatch: Colors.blue,),
-      home: LoginPage(),);
+      theme: ThemeData(primarySwatch: Colors.lightBlue,),
+      home: LoginPage(),
+    );
   }
 }
--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -8,8 +8,49 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body:Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:<Widget>[
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.number,
+                style: new TextStyle(color: Colors.white, fontSize: 20),
+                decoration: InputDecoration(
+                  labelText: "CPF",
+                  labelStyle: TextStyle(color: Colors.white)),
+              ),
+              Divider(),
+              TextFormField(
+                autofocus: true,
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                style: new TextStyle(color: Colors.white, fontSize: 20),
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(color: Colors.white)),
+              ),
+              Divider(),
+              ButtonTheme(
+                height: 60.0,
+                child: RaisedButton(
+                  onPressed:()=>{},
+                  child: Text(
+                    "Enviar",
+                    style: TextStyle(color: Colors.lightBlue),
+                  ),
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
