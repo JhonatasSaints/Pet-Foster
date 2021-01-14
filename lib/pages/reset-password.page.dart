520d6ccf99eb46de3291c5cb8ce499dcfa9a2a1f
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-01-14 01:06:59 -0300

    send button on recovery screen

--- lib/pages/reset-password.page.dart
+++ lib/pages/reset-password.page.dart
@@ -67,6 +67,33 @@ class ResetPasswordPage extends StatelessWidget {
               style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 20,),
+            Container(
+              height: 40,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end:Alignment.bottomLeft,
+                  stops:[0.3, 1],
+                  colors:[
+                    Color(0xFF64B5F6),
+                    Color(0xFFE3F2FD),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(35),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Text("Enviar", //submit buttom
+                  style: TextStyle( color: Colors.white,  fontSize: 20, fontWeight: FontWeight.w600,),
+                  textAlign: TextAlign.center,
+                  ),
+                  onPressed: () {},),
+              ),
+            ),
+            SizedBox(height: 20,),
           ],
         ),
       ),
