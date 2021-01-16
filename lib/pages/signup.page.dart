c2fffd7645717d54f67cd2692ce1edaa2899ee03
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-01-15 23:12:15 -0300

    add-user image

--- lib/pages/signup.page.dart
+++ lib/pages/signup.page.dart
@@ -4,7 +4,25 @@ class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body:Container(
+        padding: EdgeInsets.only(top:10, left:40,right:40),
+        color: Colors.lightBlue,
+        child: ListView(
+          children: <Widget>[
+            Container(
+              width: 200, height: 200,
+              alignment: Alignment(0.0, 1.15),
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: AssetImage("assets/pawblue.png"),// add-user image
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+              
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
