ede2960cafac394d86a4b1834bc89d67b4438617
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-02-22 20:16:51 -0300

    text fields on page signup

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -4,56 +4,68 @@ class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
-        padding: EdgeInsets.only(top:10, left:40,right:40),
-        color: Colors.lightBlue,
+      //button pop
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue[350],
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () => Navigator.pop(context, false),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 5, left: 40, right: 40),
+        color: Colors.white38,
         child: ListView(
           children: <Widget>[
             Container(
-              width: 200, height: 200,
+              width: 100,
+              height: 150,
               alignment: Alignment(0.0, 1.15),
               decoration: new BoxDecoration(
                 image: new DecorationImage(
-                  image: AssetImage("assets/pawblue.png"),// add-user image
+                  image: AssetImage("assets/add-user.png"), // add-user image
                   fit: BoxFit.fitHeight,
                 ),
               ),
-              child: Container(
-                height:56, width:56,
-                alignment: Alignment.center,
-                decoration:BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    stops: [0.2, 1.0],
-                    colors: [
-                      Color(0x99FFFFFF),
-                      Color(0XFFFFFFFF),
-                    ],
-                  ),
-                  border: Border.all(
-                    width: 4.0,
-                    color: const Color(0XFFFFFFFF),
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(56),
-                  ),
-                ),
-                child: SizedBox.expand(
-                  child: FlatButton(
-                    child: Icon(
-                      Icons.add,
-                      color: Colors.lightBlue,
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-              ),
+              // child: Container(
+              //   height: 56,
+              //   width: 56,
+              //   alignment: Alignment.center,
+              //   decoration: BoxDecoration(
+              //       gradient: LinearGradient(
+              //         begin: Alignment.topLeft,
+              //         end: Alignment.bottomRight,
+              //         stops: [0.2, 1.0],
+              //         colors: [
+              //           Color(0x99FFFFFF),
+              //           Color(0XFFFFFFFF),
+              //         ],
+              //       ),
+              //       border: Border.all(
+              //         width: 4.0,
+              //         color: const Color(0XFFFFFFFF),
+              //       ),
+              //       borderRadius: BorderRadius.all(
+              //         Radius.circular(56),
+              //       ),
+              //       ),
+              //   child: SizedBox.expand(
+              //     child: FlatButton(
+              //       child: Icon(
+              //         Icons.add,
+              //         color: Colors.lightBlue,
+              //       ),
+              //       onPressed: () {},
+              //     ),
+              //   ),
+              // ),
             ),
             SizedBox(
-              height:20,
+              height: 20,
             ),
-            TextFormField( //autofocus: true,
+            TextFormField(
+              //text field
               keyboardType: TextInputType.text,
               decoration: InputDecoration(
                 labelText: "Nome", //name user
@@ -68,9 +80,9 @@ class SignupPage extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 10,
+              height: 5,
             ),
-            TextFormField( //autofocus: true,
+            TextFormField(
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
                 labelText: "E-mail", //email user
@@ -85,9 +97,9 @@ class SignupPage extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 10,
+              height: 5,
             ),
-            TextFormField( //autofocus: true,
+            TextFormField(
               keyboardType: TextInputType.text,
               decoration: InputDecoration(
                 labelText: "Senha", //passord user
@@ -102,7 +114,7 @@ class SignupPage extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 10,
+              height: 5,
             ),
           ],
         ),
